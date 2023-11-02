1ed4e7af6d6cc146d3ca20029e597b9571310c7e
SickHr <blawatjustin@gmail.com>
2023-11-02 16:46:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import os.path
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 import time
 
-
-driver = webdriver.Chrome() 
+driver = webdriver.Chrome()
 
 
 def scrap():
@@ -12,22 +13,22 @@ def scrap():
     time.sleep(1)
 
     # Hol den neusten Patch
-    patch = driver.find_element(By.XPATH, "//*[@id='gatsby-focus-wrapper']/div/div[2]/div/div[1]/div/ol/li[1]/a/article/div[2]/div/h2")
+    patch = driver.find_element(By.XPATH,
+                                "//*[@id='gatsby-focus-wrapper']/div/div[2]/div/div[1]/div/ol/li[1]/a/article/div[2]/div/h2")
     patch.click()
     time.sleep(0.2)
 
     target_name = driver.find_elements(By.XPATH, "//*[@id='patch-notes-container']/div")
 
+    desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+    file_path = os.path.join(desktop_path, 'patchnotes.txt')
 
-
-    with open("", "w",encoding='utf-8') as f:
+    with open(file_path, "w", encoding='utf-8') as f:
         for name in target_name:
             f.write(name.text)
             f.write("\n" * 2)
             f.write("- - - - - - - - -")
             f.write("\n" * 2)
-
-
 
 
 scrap()
